8528e462933deb8a0f38dae3eaca6929c671d9ea
breanna <52141078+breanna-rayburn@users.noreply.github.com>
2026-03-10 21:17:58 -0700

    fix ashen

--- modules/home/nvim/plugins/ashen.nix
+++ modules/home/nvim/plugins/ashen.nix
@@ -4,10 +4,10 @@
 
   extraPlugins = with pkgs; [
     (vimUtils.buildVimPlugin {
-      name = "vim-256noir";
+      name = "ashen.nvim";
       src = fetchFromGitHub {
-        owner = "andreasvc";
-        repo = "vim-256noir";
+        owner = "ficd0";
+        repo = "ashen.nvim";
         rev = "bfb04bd50b69d863469b2deb9fb361cf0d945ba7";
         hash = "sha256-yC9V58zieE8YvEuAnJhEOgONrudUJgQFqC59cKo97/g=";
       };
