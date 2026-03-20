cd40c3586308f2f3169248cf0f0b094986d06b34
bree-lt3 <52141078+bree-lt3@users.noreply.github.com>
2026-03-20 10:36:51 -0700

    add blink-cmp keymaps

--- modules/home/nvim/plugins/blink-cmp.nix
+++ modules/home/nvim/plugins/blink-cmp.nix
@@ -6,6 +6,20 @@
       enable = true;
       settings = {
         completion.menu.draw.columns = [ [ "label" "label_description" ] ];
+        keymap = {
+          "<C-j>" = [
+            "show"
+            "select_next"
+          ];
+          "<C-k>" = [
+            "select_prev"
+            "fallback"
+          ];
+          "<C-CR>" = [
+            "select_and_accept"
+            "fallback"
+          ];
+        };
       };
     };
   };
