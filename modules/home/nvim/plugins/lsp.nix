5ed6b759ce358dce7610295420072280e9fbb9a9
bree-lt3 <52141078+bree-lt3@users.noreply.github.com>
2026-03-26 11:30:42 -0700

    update language servers

--- modules/home/nvim/plugins/lsp.nix
+++ modules/home/nvim/plugins/lsp.nix
@@ -5,8 +5,16 @@
     lsp = {
       enable = true;
       servers = {
-        nil_ls.enable = true;
+        clangd.enable = true;
         html.enable = true;
+        lua_ls.enable = true;
+        nil_ls.enable = true;
+        pyright.enable = true;
+        rust_analyzer = {
+          enable = true;
+          installCargo = false;
+          installRustc = false;
+        };
         ts_ls.enable = true;
         texlab.enable = true;
       };
