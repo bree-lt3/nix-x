70d8a347bd1bf755d381b4dcc6f3bb75a9f418d7
bree-lt3 <52141078+bree-lt3@users.noreply.github.com>
2026-04-16 12:54:38 -0700

    update swaybar battery formatting

--- modules/home/sway/bar.sh
+++ modules/home/sway/bar.sh
@@ -3,10 +3,18 @@
 # TODO: format volume to show percentage
 VOLUME=$(wpctl get-volume @DEFAULT_SINK@)
 
-# TODO: format battery show +/- for charging/dihcharging
+# Battery
 BATPERCENT=$(cat /sys/class/power_supply/BAT0/capacity)
 BATSTATUS=$(cat /sys/class/power_supply/BAT0/status)
+BATFORMAT=""
 
+if [ $BATSTATUS == "Charging" ]; then 
+  BATFORMAT="+"
+elif [ $BATSTATUS == "Discharging" ]; then
+  BATFORMAT="-"
+fi
+
+# Date
 DATETIME=$(date +"%D %T")
 
-echo [$VOLUME] [$BATPERCENT% $BATSTATUS] $DATETIME
+echo [$VOLUME] [BAT: $BATFORMAT$BATPERCENT%] $DATETIME
