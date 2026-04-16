74e23685d790b3416dfad6d940b4f82c8d4779a0
bree-lt3 <52141078+bree-lt3@users.noreply.github.com>
2026-04-16 12:31:00 -0700

    update swaybar show volume

--- modules/home/sway/bar.sh
+++ modules/home/sway/bar.sh
@@ -1,5 +1,12 @@
+# TODO: show WIFI connection if connected, show disconnected if no connection
+
+# TODO: format volume to show percentage
+VOLUME=$(wpctl get-volume @DEFAULT_SINK@)
+
+# TODO: format battery show +/- for charging/dihcharging
 BATPERCENT=$(cat /sys/class/power_supply/BAT0/capacity)
 BATSTATUS=$(cat /sys/class/power_supply/BAT0/status)
+
 DATETIME=$(date +"%D %T")
 
-echo $BATPERCENT% $BATSTATUS $DATETIME
+echo [$VOLUME] [$BATPERCENT% $BATSTATUS] $DATETIME
