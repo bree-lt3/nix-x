b6984092f5bfa3dd57ac3e4b9726fc895037030d
bree-lt3 <52141078+bree-lt3@users.noreply.github.com>
2026-09-17 17:32:09 -0700

    tlp battery stop and start

--- modules/nixos/tlp.nix
+++ modules/nixos/tlp.nix
@@ -5,6 +5,12 @@
   config = lib.mkIf config.tlp.enable {
     services.tlp = {
       enable = true;
+      settings = {
+        # start charge at 85 and below
+        START_CHARGE_THRESH_BAT0 = 85;
+        # stop charge at 90 and above
+        STOP_CHARGE_THRESH_BAT0 = 90;
+      };
     };
   };
 }
