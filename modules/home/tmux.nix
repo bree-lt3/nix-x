3456384ee285188c7d55fd73ef9181b089dbe12b
bree-lt3 <52141078+bree-lt3@users.noreply.github.com>
2026-09-22 19:29:05 -0700

    more tmux defaults

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -1,10 +1,20 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, ... }:
 
 {
   options.home.tmux.enable = lib.mkEnableOption "tmux";
   config = lib.mkIf config.home.tmux.enable {
     home-manager.users."breanna".programs.tmux = {
       enable = true;
+      prefix = "C-Space";
+      keyMode = "vi";
+      extraConfig = ''
+        set -g status-style bg=default
+
+        bind h          select-pane -L
+        bind j          select-pane -D
+        bind k          select-pane -U
+        bind l          select-pane -R
+      '';     
     };
   };
 }
